4c095673633516762aeff25a8f7f8a4e5e669e9f
leeandrich222828-prog <leeandrich222828@gmail.com>
2026-05-22 04:38:47 +0700

    update database config

--- library/database.php
+++ library/database.php
@@ -1,2 +1,13 @@
 <?php
-    $connection = new mysqli('localhost','root','','finansial');
+    $connection = new mysqli(
+        'mysql.railway.internal',
+        'root', 
+        'OHcifYURxsVCGmGDcAYttfzBRrqCcyEw',
+        'railway',
+        3306
+    );
+
+    if($connection->connect_error){
+        die("Koneksi gagal: " . $connection->connect_error);
+    }
+?>

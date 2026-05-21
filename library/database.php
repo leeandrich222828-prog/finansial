0e3f5662905a9aa2d0fdcb1b3e38157372cfd21f
leeandrich222828-prog <leeandrich222828@gmail.com>
2026-05-22 05:04:24 +0700

    fix database host

--- library/database.php
+++ library/database.php
@@ -1,11 +1,11 @@
 <?php
-    $connection = new mysqli(
-        'kodama.proxy.rlwy.net',
-        'root',
-        'OHcifYURxsVCGmGDcAYttfzBRrqCcyEw',
-        'railway',
-        30201
-    );
+    $host = getenv('MYSQLHOST') ?: 'localhost';
+    $user = getenv('MYSQLUSER') ?: 'root';
+    $pass = getenv('MYSQLPASSWORD') ?: '';
+    $db   = getenv('MYSQLDATABASE') ?: 'finansial';
+    $port = getenv('MYSQLPORT') ?: 3306;
+
+    $connection = new mysqli($host, $user, $pass, $db, $port);
 
     if($connection->connect_error){
         die("Koneksi gagal: " . $connection->connect_error);
